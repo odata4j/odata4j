b3d788658d11d0e79315c3071cc0c805030846be
John Spurlock <john.spurlock@gmail.com>
2010-04-03 23:55:52 +0000

    Added a JPAProducerExample

---
+++ odata4j-core/test/org/odata4j/examples/producer/JPAProducerExample.java
@@ -0,0 +1,28 @@
+package org.odata4j.examples.producer;
+
+import javax.persistence.Persistence;
+
+import org.odata4j.producer.jpa.JPAProducer;
+import org.odata4j.producer.resources.ODataProducerProvider;
+
+public class JPAProducerExample {
+
+    public static void main(String[] args) {
+        
+        String endpointUri = "http://localhost:8886/JPAProducerExample.svc/";
+        
+        // this example assumes you have an appropriate persistence.xml containing a valid persistence unit definition 
+        // (in this case named NorthwindService) mapping your jps entity classes, etc
+        
+        // create a JPAProducer by giving it a EntityManagerFactory
+        String persistenceUnitName = "NorthwindService";
+        String namespace = "Northwind";
+        JPAProducer producer = new JPAProducer(Persistence.createEntityManagerFactory(persistenceUnitName), namespace);
+
+        // register the producer as the static instance, then launch the http server
+        ODataProducerProvider.setInstance(producer);
+        ExampleUtil.hostODataServer(endpointUri);
+
+    }
+
+}
