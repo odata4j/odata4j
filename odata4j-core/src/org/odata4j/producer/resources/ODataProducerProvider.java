5fd665bac4249da6b98cf4ce1bf8d1905104ca38
Carl J. Mosca <carljmosca@gmail.com>
2011-04-06 03:18:57 +0000

    minor reformatting/refactoring debugging

--- odata4j-core/src/org/odata4j/producer/resources/ODataProducerProvider.java
+++ odata4j-core/src/org/odata4j/producer/resources/ODataProducerProvider.java
@@ -16,18 +16,14 @@ import com.sun.jersey.core.impl.provider.xml.LazySingletonContextProvider;
 public class ODataProducerProvider extends LazySingletonContextProvider<ODataProducer> {
 
     private final Logger log = Logger.getLogger(getClass().getName());
-    
     public static final String FACTORY_PROPNAME = "odata4j.producerfactory";
-
     private static ODataProducer STATIC;
-
-    @Context 
+    @Context
     private ResourceConfig resourceConfig;
-    
+
     public static void setInstance(ODataProducer producer) {
         STATIC = producer;
     }
-
     private ODataProducer instance;
 
     public ODataProducerProvider() {
@@ -40,16 +36,16 @@ public class ODataProducerProvider extends LazySingletonContextProvider<ODataPro
             if (STATIC != null) {
                 log.info("Setting producer instance to static instance: " + STATIC);
                 instance = STATIC;
-            } else if (resourceConfig != null && resourceConfig.getProperty(FACTORY_PROPNAME) != null){
-                String factoryTypeName =  (String) resourceConfig.getProperty(FACTORY_PROPNAME);
+            } else if (resourceConfig != null && resourceConfig.getProperty(FACTORY_PROPNAME) != null) {
+                String factoryTypeName = (String) resourceConfig.getProperty(FACTORY_PROPNAME);
                 log.info("Creating producer from factory in resource config: " + factoryTypeName);
                 Properties p = new Properties();
                 p.putAll(resourceConfig.getProperties());
-                instance = newProducerFromFactory(factoryTypeName,p);
-            } else if (System.getProperty(FACTORY_PROPNAME) != null){
+                instance = newProducerFromFactory(factoryTypeName, p);
+            } else if (System.getProperty(FACTORY_PROPNAME) != null) {
                 String factoryTypeName = System.getProperty(FACTORY_PROPNAME);
                 log.info("Creating producer from factory in system properties: " + factoryTypeName);
-                instance = newProducerFromFactory(factoryTypeName,System.getProperties());
+                instance = newProducerFromFactory(factoryTypeName, System.getProperties());
             } else {
                 throw new RuntimeException("Unable to find an OData producer implementation. Call ODataProducerProvider.setInstance to set the static singleton or set the producer factory property \'" + FACTORY_PROPNAME + "\' in either the resource config (web.xml) or system properties to a class name that implements ODataProducerFactory.");
             }
@@ -58,8 +54,9 @@ public class ODataProducerProvider extends LazySingletonContextProvider<ODataPro
     }
 
     private ODataProducer newProducerFromFactory(String factoryTypeName, Properties props) {
-        try {            
-            Class<?> factoryType = Class.forName(factoryTypeName);
+        try {
+            Class<?> factoryType;
+            factoryType = Class.forName(factoryTypeName);
             Object obj = factoryType.newInstance();
             ODataProducerFactory factory = (ODataProducerFactory) obj;
             return factory.create(props);
@@ -68,5 +65,4 @@ public class ODataProducerProvider extends LazySingletonContextProvider<ODataPro
         }
 
     }
-
 }
