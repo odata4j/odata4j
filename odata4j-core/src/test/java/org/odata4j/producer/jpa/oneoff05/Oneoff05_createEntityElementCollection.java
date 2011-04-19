120ca99a5387894fca45961033a0e7d3c06be262
Krishna Chaitanya Thota <kcthota@gmail.com>
2011-04-19 20:37:41 +0000

    test case for elementcollection annotated entity model

---
+++ odata4j-core/src/test/java/org/odata4j/producer/jpa/oneoff05/Oneoff05_createEntityElementCollection.java
@@ -0,0 +1,32 @@
+package org.odata4j.producer.jpa.oneoff05;
+
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.odata4j.consumer.ODataConsumer;
+import org.odata4j.core.OEntity;
+import org.odata4j.core.OProperties;
+import org.odata4j.producer.jpa.oneoff.OneoffTestBase;
+
+public class Oneoff05_createEntityElementCollection extends OneoffTestBase {
+
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+		setUpClass(Oneoff05_createEntityElementCollection.class,20);		
+	}
+	
+	@Ignore
+	@Test
+	public void createEntityElementCollection()	
+	{
+		final long now = System.currentTimeMillis();
+		ODataConsumer consumer = ODataConsumer.create(endpointUri);
+		
+			
+		OEntity student = consumer
+		.createEntity("Student")
+		.properties(OProperties.string("StudentName", "Student1" + now))
+		//todo add courses as well to the student
+		.execute();
+	}
+}
