6ebe5a71cf5dc4ccee896853dce3e17af6badc8c
Tony Rozga <tony.rozga@gmail.com>
2011-10-27 10:47:26 -0600

    experiment to support inheritance during parsing of inlined entities

--- odata4j-core/src/main/java/org/odata4j/format/json/JsonFormatParser.java
+++ odata4j-core/src/main/java/org/odata4j/format/json/JsonFormatParser.java
@@ -21,6 +21,7 @@ import org.odata4j.edm.EdmCollectionType;
 import org.odata4j.edm.EdmComplexType;
 import org.odata4j.edm.EdmDataServices;
 import org.odata4j.edm.EdmEntitySet;
+import org.odata4j.edm.EdmEntityType;
 import org.odata4j.edm.EdmMultiplicity;
 import org.odata4j.edm.EdmNavigationProperty;
 import org.odata4j.edm.EdmProperty;
@@ -98,7 +99,26 @@ public class JsonFormatParser {
   }
 
   protected JsonEntry parseEntry(JsonEntryMetaData jemd, EdmEntitySet ees, JsonStreamReader jsr) {
-    JsonEntry entry = new JsonEntry();
+    
+    // does the metadata refine the type of entity to expect?
+    if (null != jemd && jemd.type != null && !ees.getType().getFullyQualifiedTypeName().equals(jemd.type)) {
+      // yes it does.  Now we run into the next issue:  We don't have an EdmEntitySet foreach
+      // subclass that may show up.  This bleeds all the way out to the OEntity interface.
+      // It has a single getEntitySet() when it really needs getEntitySet() and getEntityType() that are not coupled.
+      // how about creating a temp EdmEntitySet...
+      
+      EdmEntityType refinedType = (EdmEntityType) this.metadata.findEdmEntityType(jemd.type);
+      if (null == refinedType) {
+        throw new IllegalArgumentException("failed resolving type: " + jemd.type);
+      }
+      
+      // god this builder stuff is going to drive me nuts.
+      ees = EdmEntitySet.newBuilder(refinedType.getFullyQualifiedTypeName(), this.metadata)
+              .setName(ees.getName())
+              .build();
+    }
+    
+    JsonEntry entry = new JsonEntry(ees);
     entry.properties = new ArrayList<OProperty<?>>();
     entry.links = new ArrayList<OLink>();
     while (jsr.hasNext()) {
@@ -115,7 +135,7 @@ public class JsonFormatParser {
   }
 
   protected JsonEntry parseEntry(EdmEntitySet ees, JsonStreamReader jsr) {
-    JsonEntry entry = new JsonEntry();
+    JsonEntry entry = new JsonEntry(ees);
     entry.properties = new ArrayList<OProperty<?>>();
     entry.links = new ArrayList<OLink>();
 
@@ -334,6 +354,16 @@ public class JsonFormatParser {
       // inlined entity or link starting with meta data
       EdmNavigationProperty navProp = ees.getType().findNavigationProperty(name);
       JsonEntryMetaData jemd = parseMetadata(jsr);
+      
+      /*
+       * Entity Inheritance Hierarchy Issue:  the navigation property ToRole.type
+       * may reference a base entity type.  The object(s) on the to end of the
+       * actual relationship can be of that type or any sub-type.
+       * The __metadata section just parsed should have a type: property that gives
+       * the actual type of the object we need to parse next.
+       * 
+       * 
+       */
       JsonEntry refentry = parseEntry(jemd, metadata.getEdmEntitySet(navProp.getToRole().getType()), jsr);
 
       // if we are parsing a request, the links to existing
@@ -356,7 +386,7 @@ public class JsonFormatParser {
       if (null != navProp) {
         ees = metadata.getEdmEntitySet(navProp.getToRole().getType());
 
-        JsonEntry refentry = new JsonEntry();
+        JsonEntry refentry = new JsonEntry(ees);
         refentry.properties = new ArrayList<OProperty<?>>();
         refentry.links = new ArrayList<OLink>();
         do {
