dc42529d785aa1b3afca28c759595a233aaea884
Tony Rozga <tony.rozga@gmail.com>
2012-04-20 11:44:00 -0600

    forgot to add Namespace2.java

---
+++ odata4j-core/src/main/java/org/odata4j/stax2/Namespace2.java
@@ -0,0 +1,12 @@
+
+package org.odata4j.stax2;
+
+/**
+ */
+public interface Namespace2 extends Attribute2 {
+
+  String getNamespaceURI();
+  String getPrefix();
+  boolean isDefaultNamespaceDeclaration();
+  
+}
