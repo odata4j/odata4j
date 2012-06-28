74e84f3bbb4bec039f49e687398509d5a3b94ef7
Tony Rozga <tony.rozga@gmail.com>
2012-06-28 15:57:02 -0600

    - whoops, didn't have all my changes in the last push

--- odata4j-fit/src/test/java/org/odata4j/test/integration/producer/custom/CustomTest.java
+++ odata4j-fit/src/test/java/org/odata4j/test/integration/producer/custom/CustomTest.java
@@ -275,7 +275,6 @@ public class CustomTest extends CustomBaseTest {
   }
 
   @Test
-  @Ignore
   public void testCreateMLE() throws InterruptedException {
     /**
      * There appears to be a strange race condition or something in the test environment:
@@ -297,7 +296,6 @@ public class CustomTest extends CustomBaseTest {
   }
 
   @Test
-  @Ignore
   public void testUpdateMLE() {
      /**
      * There appears to be a strange race condition or something in the test environment:
@@ -305,13 +303,13 @@ public class CustomTest extends CustomBaseTest {
      * timeout waiting for data from the client.  Not sure why or if it is a client
      * or server issue. Workaround:  issue a GET first to prime things.
      */
-    String contentBefore = rtFacade.getWebResource(endpointUri + "MLEs('ANewMLE')/$value" + "?$format=json").getEntity();
+    String contentBefore = rtFacade.getWebResource(endpointUri + "MLEs('foobar')/$value" + "?$format=json").getEntity();
     
     String content = "This MLE was updated by the test testUpdateMLE()";
-    int status = rtFacade.putWebResource(endpointUri + "MLEs('ANewMLE')", new ByteArrayInputStream(content.getBytes()), MediaType.TEXT_PLAIN_TYPE, null).getStatusCode();
+    int status = rtFacade.putWebResource(endpointUri + "MLEs('foobar')", new ByteArrayInputStream(content.getBytes()), MediaType.TEXT_PLAIN_TYPE, null).getStatusCode();
     assertEquals(Status.OK.getStatusCode(), status);
 
-    String content2 = rtFacade.getWebResource(endpointUri + "MLEs('ANewMLE')/$value" + "?$format=json").getEntity();
+    String content2 = rtFacade.getWebResource(endpointUri + "MLEs('foobar')/$value" + "?$format=json").getEntity();
     assertEquals(content, content2);
   }
 
